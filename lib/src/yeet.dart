02b4e0f647f1cc1067c9e0b1c09983b448834777
Hossein Yousefi <hossein@identi.app>
2021-03-03 01:41:46 +0100

    removing persist for now

--- lib/src/yeet.dart
+++ lib/src/yeet.dart
@@ -27,12 +27,10 @@ class Yeet {
   final List<Yeet>? children;
   final List<String> parameters;
   late final RegExp? regExp;
-  final bool persist;
 
   Yeet({
     this.path,
     bool caseSensitive = true,
-    this.persist = false,
     this.builder,
     this.children,
   }) : parameters = [] {
